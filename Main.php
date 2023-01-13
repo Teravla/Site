fa3f667db6fcf3d3bbb05b3f7e395b9c1cc81190
Teravla <75727791+Teravla@users.noreply.github.com>
2023-01-13 09:53:27 +0100

    modified:   .vscode/launch.json 	new file:   .vscode/tasks.json 	new file:   Main.php 	modified:   projet.html

---
+++ Main.php
@@ -0,0 +1,12 @@
+/*
+<?php
+    $name = $_POST['name'];
+    $desc = $_POST['description'];
+    $corps = $_POST['corps'];
+
+    $file = "Stockage_Donnees.txt";
+    $data = $name . ',' . $desc . ';' . $corps . "\n";
+    file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
+
+?>
+*/
