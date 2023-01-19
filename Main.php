6a6d7efe38e0f2c32f81ac3641d40d0689dc3e5e
Teravla <75727791+Teravla@users.noreply.github.com>
2023-01-19 19:16:14 +0100

    modified:   .vscode/tasks.json 	modified:   Main.php 	new file:   Stockage_Donnees.txt 	modified:   about_us.css 	modified:   master.css 	modified:   projet.html

--- Main.php
+++ Main.php
@@ -1,12 +1,49 @@
-/*
 <?php
-    $name = $_POST['name'];
-    $desc = $_POST['description'];
-    $corps = $_POST['corps'];
 
-    $file = "Stockage_Donnees.txt";
-    $data = $name . ',' . $desc . ';' . $corps . "\n";
-    file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
+        
+        $user = 'root';
+        $password = 'root';
+        $db = 'inventory';
+        $host = 'localhost';
+        $port = 3306;
+
+        $link = mysqli_init();
+        $success = mysqli_real_connect(
+                $link, 
+                $host, 
+                $user, 
+                $password, 
+                $db,
+                $port
+        );
+
+
+        if (isset($_POST['projet_name']))
+        {
+            $name =  $_POST['projet_name'];
+        }
+        if (isset($_POST['projet_description']))
+        {
+            $description =  $_POST['projet_description'];
+        }
+        if (isset($_POST['projet_corps']))
+        {
+            $corps =  $_POST['projet_corps'];
+        }
+
+        if (isset($_POST['team']))
+        {
+            $team =  $_POST['team'];
+        }
+
+        $content = "Nom du projet : " . $name . "\n" . "Description du projet : " . $description . "\n" . "Pour plus de detail : " . $corps . "\n\n" . "Equipe concerné " . $team;
+
+        file_put_contents('Stockage_Donnees.txt', $content);
+
+        echo "C'est bon"
+
+
+
+
 
 ?>
-*/
